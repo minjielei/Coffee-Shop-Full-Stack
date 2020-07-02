bf3b07d0ead33dc231591f853618f9ccd06cfe35
minjie <minjilei@umich.edu>
2020-07-02 17:10:38 -0400

    implemented all endpoints

--- backend/src/api.py
+++ backend/src/api.py
@@ -12,21 +12,31 @@ setup_db(app)
 CORS(app)
 
 '''
-@TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 ## ROUTES
 '''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
+GET /drinks
+    it should be a public endpoint
+    it should contain only the drink.short() data representation
+returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks')
+def get_drinks():
+    try:
+        drinks = [drink.short() for drink in Drink.query.order_by(Drink.id).all()]
+
+        return jsonify({
+                'success': True,
+                'drinks': drinks
+            })
+
+    except:
+        abort(422)
 
 
 '''
@@ -37,10 +47,23 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(payload):
+    try:
+        drinks = [drink.long() for drink in Drink.query.order_by(Drink.id).all()]
+
+        return jsonify({
+                'success': True,
+                'drinks': drinks
+            })
+
+    except:
+        abort(422)
 
 
 '''
-@TODO implement endpoint
+implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
@@ -48,10 +71,26 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def add_drinks(payload):
+    body = request.get_json()
+    try:
+        title = body.get('title')
+        recipe = json.dumps(body.get('recipe'))
+        drink = Drink(title=title, recipe=recipe)
+        drink.insert()
 
+        return jsonify({
+                'success': True,
+                'drinks': [drink.long()]
+            })
+
+    except:
+        abort(422)
 
 '''
-@TODO implement endpoint
+implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -61,10 +100,31 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(payload, drink_id):
+    body = request.get_json()
+    try:
+        drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+        if drink is None:
+            abort(404)
+        title = body['title']
+        recipe = json.dumps(body.get('recipe'))
+        drink.title = title
+        drink.recipe = recipe
+        drink.update()
+
+        return jsonify({
+            "success": True,
+            "drinks": [drink.long()]
+        })
+
+    except:
+        abort(422)
 
 
 '''
-@TODO implement endpoint
+implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -73,6 +133,23 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, drink_id):
+    try:
+        drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+
+        if drink is None:
+            abort(404)
+        drink.delete()
+
+        return jsonify({
+            'success': True,
+            'delete': drink_id
+        })
+
+    except:
+        abort(422)
 
 
 ## Error Handling
@@ -87,24 +164,47 @@ def unprocessable(error):
                     "message": "unprocessable"
                     }), 422
 
+
 '''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
+implement error handler for 400
+'''
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        'success': False,
+        'error': 400,
+        'message': 'Bad request'
+    }), 400
+
+'''
+implement error handler for 401
+'''
+@app.errorhandler(401)
+def not_authorized(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "Not authorized"
+    }), 401
+
+'''
+implement error handler for 404
+    error handler should conform to general task above 
+'''
+@app.errorhandler(404)
+def notfound(error):
+    return jsonify({
                     "success": False, 
                     "error": 404,
                     "message": "resource not found"
                     }), 404
 
 '''
-
-'''
-@TODO implement error handler for 404
+implement error handler for AuthError
     error handler should conform to general task above 
 '''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
